0e8412f717c8dab13e42bb4d1dfe17776b77f92f
Jeff juhui-pc <330132662@qq.com>
2022-08-12 16:40:13 +0800

    1

---
+++ app/src/main/java/com/juhui/gas/ui/ui/login/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.juhui.gas.ui.ui.login;
+
+import android.os.Bundle;
+
+import com.hjq.demo.R;
+import com.hjq.demo.app.AppActivity;
+
+public class LoginActivity extends AppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login2;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
